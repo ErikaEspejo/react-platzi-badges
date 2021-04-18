a8c9de6ae82e8b352f8f1b38701d59cc54598001
Erika Espejo <erikaespejo95@gmail.com>
2021-04-17 20:36:33 -0500

    badge component props

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -3,7 +3,7 @@
   - Un componente es una clase, por lo cual debe heredar del objeto React.Component
   - Debe manejar al menos un metodo, render() que muestra lo que el navegador debe renderizar
   - Se debe exportar el componente (similar a como ocurre con los modulos)
-
+  - Props: atributos de un componente
 */
 
 import React from 'react';
@@ -12,18 +12,21 @@ import confLogo from '../images/badge-header.svg' //al importar la imagen en con
 
 class Badge extends React.Component {
   render() {
+
+
+
     return (
       <div className = "Badge">
         <div className = "Badge__header">
           <img src={confLogo} alt="Logo de la conferencia"/> {/* React genera una direccion para la imagen con una especie de hash */}
         </div>
         <div className = "Badge__section-name">
-          <img className = "Badge__avatar" src="https://www.gravatar.com/userimage/205043051/a0fdffa22d693bbd759fd543ac0b5752?size=120" alt="Avatar"/> {/* gravar sirve para poner imagenes de avatar, es muy usado */}
-          <h1>Erika <br/> Espejo </h1>
+          <img className = "Badge__avatar" src={this.props.avatarUrl} alt="Avatar"/> {/* gravar sirve para poner imagenes de avatar, es muy usado */}
+          <h1> {this.props.firstName} <br/> {this.props.lastName} </h1>
         </div>
         <div className = "Badge__section-info">
-          <h4>Desarrolladora web </h4>
-          <p>@Erika__Espejo</p>
+          <h4> {this.props.jobTitle} </h4>
+          <p>@{this.props.twitter}</p>
         </div>
         <div className = "Badge__footer">
          <p> #platziconf </p>
--- src/index.js
+++ src/index.js
@@ -7,4 +7,13 @@ import Badge from './components/Badge';
 
 const container = document.getElementById('app'); //app se convierte en el contenedor
 
-ReactDOM.render(<Badge />,container); //obtiene el que renderiza y en donde
+ReactDOM.render(
+  <Badge
+    firstName = 'Lilly'
+    lastName = 'Hola'
+    jobTitle = "Web Developer"
+    twitter = "Erika__Espejo"
+    avatarUrl = "https://www.gravatar.com/userimage/205043051/a0fdffa22d693bbd759fd543ac0b5752?size=120"
+  />,
+  container
+); //obtiene el que renderiza y en donde
