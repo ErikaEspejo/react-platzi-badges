dcd6666ee49b7e395a70c4485bdafa42a61a263b
Erika Espejo <erikaespejo95@gmail.com>
2021-04-18 14:47:31 -0500

    levantamiento de estado - badgeForm & newBadge

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -2,19 +2,18 @@ import React from 'react';
 
 
 class BadgeForm extends React.Component {
-  state = {};
 
-  handleChange = (e) => {
+  //handleChange = (e) => {
     /* console.log({
       name: e.target.name,
       value: e.target.value,
     }); */
 
-    this.setState({
+    /* this.setState({
       [e.target.name]: e.target.value,
 
     });
-  };
+  }; */
 
   handleClick = (e) => {
     console.log('Button was clicked')
@@ -33,23 +32,23 @@ class BadgeForm extends React.Component {
         <form action="" onSubmit = {this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="">First Name</label>
-            <input onChange={this.handleChange} className="form-control" type="text" name="firstName" value={this.state.firstName} />
+            <input onChange={this.props.onChange} className="form-control" type="text" name="firstName" value={this.props.formValues.firstName} />
           </div>
           <div className="form-group">
             <label htmlFor="">Last Name</label>
-            <input onChange={this.handleChange} className="form-control" type="text" name="lastName" value={this.state.lastName}/>
+            <input onChange={this.props.onChange} className="form-control" type="text" name="lastName" value={this.props.formValues.lastName}/>
           </div>
           <div className="form-group">
             <label htmlFor="">Email</label>
-            <input onChange={this.handleChange} className="form-control" type="email" name="email" value={this.state.email}/>
+            <input onChange={this.props.onChange} className="form-control" type="email" name="email" value={this.props.formValues.email}/>
           </div>
           <div className="form-group">
             <label htmlFor="">Job Title</label>
-            <input onChange={this.handleChange} className="form-control" type="text" name="jobTitle" value={this.state.jobTitle}/>
+            <input onChange={this.props.onChange} className="form-control" type="text" name="jobTitle" value={this.props.formValues.jobTitle}/>
           </div>
           <div className="form-group">
             <label htmlFor="">Twitter</label>
-            <input onChange={this.handleChange} className="form-control" type="text" name="twitter" value={this.state.twitter}/>
+            <input onChange={this.props.onChange} className="form-control" type="text" name="twitter" value={this.props.formValues.twitter}/>
           </div>
           <button onClick={this.handleClick} className="btn btn-primary"> {/* existen dos maneras para manejar el submit por defecto de un boton, puede ser dentro de la etiqueta usando type="button", o dejarlo como por defecto y agregar evento onSubmit = {this.handleSubmit} en el form. */}
             Save
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -7,6 +7,25 @@ import BadgeForm from '../components/BadgeForm';
 import Badge from '../components/Badge';
 
 class BadgeNew extends React.Component {
+  state = { form:{
+      firstName: '',
+      lastName: '',
+      jobTitle: '',
+      email: '',
+      twitter: '',
+    }
+  };
+
+
+  handleChange = e => {
+    this.setState({
+      form: {
+        ...this.state.form, //vienen todos los valores del form y se añade otro
+        [e.target.name] : e.target.value,
+      }
+    })
+  }
+
   render() {
     return(
       <div>
@@ -20,15 +39,16 @@ class BadgeNew extends React.Component {
           <div className="row">
             <div className="col">
               <Badge
-                firstName = 'Erika'
-                lastName = 'Espejo'
-                jobTitle = "Web Developer"
-                twitter = "Erika__Espejo"
+                firstName = {this.state.form.firstName}
+                lastName = {this.state.form.lastName}
+                jobTitle = {this.state.form.jobTitle}
+                twitter = {this.state.form.twitter}
+                email = {this.state.form.email}
                 avatarUrl = "https://www.gravatar.com/userimage/205043051/a0fdffa22d693bbd759fd543ac0b5752?size=120"
               />
             </div>
             <div className="col">
-              <BadgeForm/>
+              <BadgeForm onChange = {this.handleChange} formValues = {this.state.form}/>
             </div>
           </div>
         </div>
